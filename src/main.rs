1f747424638064d42bf05249408a3cfe40b8286b
Shekhar <skjha98@gmail.com>
2025-06-08 02:11:25 +0530

    remove_code_block: replace_all not working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use std::{fs, path::PathBuf};
 use anyhow::{Context, Result};
+use regex::Regex;
 
 #[derive(Parser)]
 #[command(author, version, about)]
@@ -14,6 +15,21 @@ fn main() -> Result<()>{
 
     let content = fs::read_to_string(&args.file_path).with_context(|| format!("Failed to read {}", args.file_path.display()))?;
 
-    println!("File content:\n{}", content);
+    println!("File content:\n{}", &content);
+
+    let word_count = count_words(&content);
+
+    println!("Word count:\n{}", word_count);
     Ok(())
 }
+
+fn remove_code_block(content: &str) -> String {
+    let re = Regex::new("```.*?```").unwrap();
+    re.replace_all(content, "").to_string()
+}
+
+fn count_words(content: &str) -> usize {
+    let cleared_context = remove_code_block(content);
+    cleared_context.split_whitespace().map(|x| println!("{}", x)).count()
+}
+
--- test.md
+++ test.md
@@ -1,1 +1,16 @@
-echo "# Hello\nThis is a test." > test.md
+
+# This is heading 1
+## This is heading 2
+
+` This is strikethrough `
+* This is a pointer
+
+```rust
+// This is a code block
+fn main() {
+    // This is main function 12
+    println!("This is the print message");
+}
+```
+
+May be this should be fine?
