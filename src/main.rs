3e331d71741ea1e5b32a1acae2572568c324adc2
Shekhar <skjha98@gmail.com>
2025-06-10 00:53:26 +0530

    Added json support

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,25 @@ use clap::Parser;
 use std::{fs, path::PathBuf};
 use anyhow::{Context, Result};
 use regex::Regex;
+use serde::Serialize;
 
 #[derive(Parser)]
 #[command(author, version, about)]
 struct Args {
-    /// Path to Markdown json
+    /// Path to Markdown
     file_path: PathBuf,
+
+    /// Output as json
+    #[arg(short, long)]
+    json: bool
+}
+
+#[derive(Serialize)]
+struct MarkdownStats {
+    word_count: usize,
+    headers: Vec<(usize, String)>,
+    code_blocks: usize,
+    links: usize,
 }
 
 fn main() -> Result<()>{
@@ -15,14 +28,26 @@ fn main() -> Result<()>{
 
     let content = fs::read_to_string(&args.file_path).with_context(|| format!("Failed to read {}", args.file_path.display()))?;
 
+    if content.trim().is_empty() {
+        println!("⚠️ File is empty");
+        return Ok(());
+    }
+
     // println!("File content:\n{}", &content);
 
-    let word_count = count_words(&content);
-    let extracted_header = extract_headers(&content);
-    let links_count = count_urls(&content);
-    let code_block_count = count_code_blocks(&content);
+    let stats: MarkdownStats = MarkdownStats {
+        word_count: count_words(&content),
+        headers: extract_headers(&content),
+        code_blocks: count_code_blocks(&content),
+        links: count_urls(&content),
+    };
 
-    print_stats(word_count, &extracted_header, code_block_count, links_count);
+    if args.json == true {
+        println!("{}", serde_json::to_string_pretty(&stats)?);
+    }
+    else {
+        print_stats(stats.word_count, &stats.headers, stats.code_blocks, stats.links);
+    }
 
     Ok(())
 }
@@ -56,11 +81,11 @@ fn count_code_blocks(content: &str) -> usize {
     re.find_iter(content).count()
 }
 
-fn print_stats(words_count: usize, headers: &[(usize, String)], code_blocks: usize, links: usize) {
+fn print_stats(word_count: usize, headers: &[(usize, String)], code_blocks: usize, links: usize) {
     println!("📊 Markdown Analysis");
     println!("----------------------");
 
-    println!(". Words: {}", words_count);
+    println!(". Words: {}", word_count);
 
     let mut headers_count = [0;6];
     for (level, _) in headers {
@@ -68,9 +93,17 @@ fn print_stats(words_count: usize, headers: &[(usize, String)], code_blocks: usi
             headers_count[*level - 1] += 1;
         }
     }
-    println!(". Headers:");
-    for (level, count) in headers_count.iter().enumerate() {
-        println!("\t- h{}: {}", level+1, count);
+    
+    if headers.is_empty() {
+        println!(". Headers: None");
+    }
+    else {
+        println!(". Headers:");
+        for (level, count) in headers_count.iter().enumerate() {
+            if *count > 0 {
+                println!("\t- h{}: {}", level+1, count);
+            }
+        }
     }
     println!(". Code blocks: {}", code_blocks);
     println!(". Links: {}", links);
